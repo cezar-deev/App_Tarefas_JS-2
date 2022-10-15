33239851564bce413182bc486c28aa4ea3266145
Cezar Cardoso <cezar.deev@gmail.com>
2022-10-15 19:08:21 -0300

    ok

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,23 @@
-console.log("teste JS")
+
+// Função que adiciona tarefa 
+function addtask() {
+
+    //titulo da tarefa
+    const taskTitle = document.querySelector("#task-title").value;
+    
+    
+
+}
+
+
+// Evento de adicionar Tarefa
+const addBtn = document.querySelector("#add-btn");
+
+addBtn.addEventListener("click", function(e) {
+
+  //Aguarda uma ação a ser feita no Formulario  
+  e.preventDefault();
+
+  addtask();
+
+});
