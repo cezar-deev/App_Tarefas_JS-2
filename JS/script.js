6dcf620273e9bb0e8ff77efe4900de93594286f8
Cezar Cardoso <cezar.deev@gmail.com>
2022-10-15 19:24:13 -0300

    ok

--- JS/script.js
+++ JS/script.js
@@ -2,10 +2,29 @@
 // Função que adiciona tarefa 
 function addtask() {
 
-    //titulo da tarefa
+    // titulo da tarefa
     const taskTitle = document.querySelector("#task-title").value;
     
-    
+    if(taskTitle) {
+
+        // clonar template
+        const template = document.querySelector(".template");
+        const newTask = template.cloneNode(true);
+
+        // Adicona titulo
+        newTask.querySelector(".task-title").textContent = taskTitle;
+
+        // remover as classes desnecessárias
+        newTask.classList.remove("template")
+        newTask.classList.remove("hide")
+
+        // adicionar tarefa na lista
+        const list =  document.querySelector("#task-list");
+        list.appendChild(newTask)
+        
+        // tlimpar texto
+        document.querySelector("#task-title").value = "";
+    }
 
 }
 
